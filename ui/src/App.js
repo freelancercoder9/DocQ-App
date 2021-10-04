38716458cfd152b6f1fa6496c3760887fe56f0de
demo <hello@gmail.com>
2021-10-04 14:26:03 +0800

    deleted unwanted files

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,8 +13,8 @@ import RejectedFilesScreen from "./screens/RejectedFilesScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 import HomePage from "./screens/HomePage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import RightSignUp from "./components1/RightSignUp";
 import SignUpOrg2 from "./screens/SignUpOrg2";
+import DashboardLeftPane from "./components/DashboardLeftPane";
 
 function App() {
   return (
@@ -24,33 +24,38 @@ function App() {
           <Route path="/" component={HomePage} exact={true}></Route>
           <Route path="/signin" component={SignIn} exact={true}></Route>
           <Route path="/signup" component={BeforeSignUp} exact={true}></Route>
-          <Route path="/dashboardScreen" component={DashboardScreen} exact={true}></Route>
-          <Route path="/signUpIndividual" component={SignUpIndividual} exact={true}></Route>
-          <Route path="/signUpOrganisation" component={SignUpOrganisation} exact={true}></Route>
+          <Route
+            path="/dashboardScreen"
+            component={DashboardScreen}
+            exact={true}
+          ></Route>
+          <Route
+            path="/signUpIndividual"
+            component={SignUpIndividual}
+            exact={true}
+          ></Route>
+          <Route
+            path="/signUpOrganisation"
+            component={SignUpOrganisation}
+            exact={true}
+          ></Route>
           <Route path="/signUpOrg2" component={SignUpOrg2} exact={true}></Route>
           <Route path="/forgotPwd" component={ForgotPwd} exact={true}></Route>
           <Route path="/homePage" component={HomePage} exact={true}></Route>
-          <Route path="/documents" component={GroupFilesScreen} exact={true}></Route>
+          <Route
+            path="/documents"
+            component={GroupFilesScreen}
+            exact={true}
+          ></Route>
           <Route path="/upload" component={UploadFiles} exact={true}></Route>
-          <Route path="/users" component={ManageUsersScreen} exact={true}></Route>
+          <Route
+            path="/users"
+            component={ManageUsersScreen}
+            exact={true}
+          ></Route>
           <Route path="/newJoinee" component={NewJoinee} exact={true}></Route>
         </Switch>
       </Router>
-      {/* <BeforeSignUp></BeforeSignUp> */}
-      {/* <SignUpIndividual></SignUpIndividual> */}
-      {/* <SignUpOrganisation></SignUpOrganisation> */}
-      {/* <SignIn></SignIn> */}
-      {/* <ForgotPwd></ForgotPwd> */}
-      {/* <UploadFiles></UploadFiles> */}
-      {/* <ShareFiles></ShareFiles> */}
-      {/* <UserSettings></UserSettings> */}
-      {/* <NewJoinee></NewJoinee> */}
-      {/* <ManageUsersScreen></ManageUsersScreen> */}
-      {/* <GroupFilesScreen></GroupFilesScreen> */}
-      {/* <RejectedFilesScreen></RejectedFilesScreen> */}
-      {/* <DashboardScreen></DashboardScreen> */}
-      {/* <HomePage></HomePage> */}
-      {/* <SignUpOrg2></SignUpOrg2> */}
     </div>
   );
 }
