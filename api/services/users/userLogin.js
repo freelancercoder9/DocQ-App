b6f684bd08389111c113730c468f8c1bddf2b621
Chandra Pasumarthi <chandra_pasumarthi@sg.smbc.co.jp>
2021-10-06 19:17:51 +0800

    User registarion, login and delete API's implemented.

--- api/services/users/userLogin.js
+++ api/services/users/userLogin.js
@@ -3,13 +3,77 @@
 // * Author: chanduthedev@gmail.com
 // ------------------------------------------------
 
-("use strict");
+"use strict";
 
-async function process(req, res) {
-  return res.status(201).json({
-    data: {},
-    code: 200,
-    message: "User Login Success",
-  });
+const Users = require("../../models/user");
+const commonResponseCodes = require("../../responses/commonRespCodes");
+const commErrorCodes = require("../../responses/commonErrorCodes");
+const validations = require("../../utils/validations");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+});
+
+async function processRequest(req, res) {
+  let reqBody = req.body;
+  const { email, password } = reqBody;
+
+  try {
+    const result = validations.validateLoginUserRequestBody(reqBody);
+    if (result["status"] !== commErrorCodes.SUCCESS.status) {
+      return res.status(result["status"]).json({
+        code: result["code"],
+        message: result["message"],
+      });
+    }
+
+    let userDetails = await Users.findOne({ email: email });
+
+    if (userDetails) {
+      const validPassword = await validations.validatePassword(
+        password,
+        userDetails.password
+      );
+
+      if (!validPassword) {
+        return res.status(400).json({
+          code: commonResponseCodes.INVALID_CREDENTIALS.code,
+          message: commonResponseCodes.INVALID_CREDENTIALS.message,
+        });
+      }
+
+      const accessToken = jwt.sign(
+        { userName: email },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
+      let respData = {};
+      respData["firstName"] = userDetails.first_name;
+      respData["lastName"] = userDetails.last_name;
+      respData["email"] = userDetails.email;
+      respData["mobileNo"] = userDetails.mobile_no;
+      respData["role"] = userDetails.role;
+      respData["status"] = userDetails.status;
+      respData["createdTimestamp"] = userDetails.created_timestamp;
+      respData["accessToken"] = accessToken;
+
+      return res.status(200).json({
+        data: respData,
+        message: commonResponseCodes.LOGIN_SUCCESS.message,
+        code: commonResponseCodes.LOGIN_SUCCESS.code,
+      });
+    } else {
+      return res.status(404).json({
+        code: commonResponseCodes.USER_DOESNOT_EXISTS.code,
+        message: commonResponseCodes.USER_DOESNOT_EXISTS.message,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 }
-exports.process = process;
+exports.processRequest = processRequest;
---
+++ api/utils/constants.js
@@ -0,0 +1,14 @@
+// ------------------------------------------------
+// * Application: DocQ App
+// * Author: chanduthedev@gmail.com
+// ------------------------------------------------
+
+const appConfig = require("../appConfig").configuration;
+module.exports = {
+  userStatusList: [
+    appConfig.userStatus.approved,
+    appConfig.userStatus.pending,
+    appConfig.userStatus.rejected,
+  ],
+  roles: [appConfig.roles.admin, appConfig.roles.staff],
+};
