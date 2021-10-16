3103b17302ba2baede251084f5a5f4731b508252
demo <hello@gmail.com>
2021-10-16 22:04:00 +0800

    created test case

--- ui/src/services/OrgService.js
+++ ui/src/services/OrgService.js
@@ -25,3 +25,29 @@ export async function createOrgName(orgCreateData) {
 
   return res;
 }
+export async function deleteOrgName(orgName) {
+  console.log(`In request of create Org ${orgName}`);
+  var newUrl = "http://192.168.18.3:7777/org/" + orgName;
+  const res = axios
+    .delete(newUrl)
+    .then((response) => {
+      console.log(`Response on Success : ${response.data}`);
+      return {
+        returnCode: 0,
+        statusCode: response.data.code,
+        message: response.data.message,
+        data: response.data.data,
+      };
+    })
+    .catch((error) => {
+      console.log(`Response on Error : ${error}`);
+      console.log(error.response.data);
+      return {
+        returnCode: -1,
+        statusCode: error.response.data.code,
+        message: error.response.data.message,
+      };
+    });
+
+  return res;
+}
--- ui/src/test/OrgServices.test.js
+++ ui/src/test/OrgServices.test.js
@@ -5,9 +5,8 @@ import * as services from "../services/OrgService";
 test("renders learn react link", () => {
   expect("hello").toBe("hello");
 });
-
-test("createOrgTestCase", async () => {
-  var ogInfo = {
+test("createOrg successfully", async () => {
+  var orgInfo = {
     organisationName: "orgName5",
     email: "testEmail5@gmail.com",
     mobileNo: "1234567899",
@@ -16,11 +15,40 @@ test("createOrgTestCase", async () => {
     city: "HYD",
     membersCount: 10,
   };
-  const response = await services.createOrgName(ogInfo);
+  const response = await services.createOrgName(orgInfo);
+  console.log(response);
+  expect({
+    returnCode: 0,
+    message: "Organization created successfully",
+    statusCode: 20006,
+  }).toStrictEqual(response);
+});
+
+test("createOrgTestCase already exist", async () => {
+  var orgInfo = {
+    organisationName: "orgName5",
+    email: "testEmail5@gmail.com",
+    mobileNo: "1234567899",
+    country: "INDIA",
+    state: "TL",
+    city: "HYD",
+    membersCount: 10,
+  };
+  const response = await services.createOrgName(orgInfo);
   console.log(response);
   expect({
     returnCode: -1,
     message: "Organization name already exists.",
     statusCode: 40006,
+  }).toStrictEqual(response);
+});
+
+test("deleteOrgTestCase", async () => {
+  const response = await services.deleteOrgName("testEmail5@gmail.com");
+  console.log(response);
+  expect({
+    returnCode: 0,
+    message: "Organization details deleted successfully",
+    statusCode: 20006,
   }).toStrictEqual(response);
 });
